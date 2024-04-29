3dd076d4779fe8274159110506f7033824247e37
Arsenal204 <diegorobles031204@gmail.com>
2024-04-29 23:11:49 +0200

    Update ListaDietasDAO.java

--- src/main/java/frutasEnForma/model/ListaDietasDAO.java
+++ src/main/java/frutasEnForma/model/ListaDietasDAO.java
@@ -1,5 +1,29 @@
 package frutasEnForma.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ListaDietasDAO {
 
+	public static String añadirDieta (int idListaDietas, Connection con ) {
+		
+		
+		try {
+		String query = "INSERT INTO listadietas idListaDietas=? nombre=? numComidas=?)";
+			PreparedStatement pstmt = con.prepareStatement(query);
+			
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
 }
